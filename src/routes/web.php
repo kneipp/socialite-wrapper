17df800cfac4a88b51b9922cdbd4ab22aeb399bb
Fabrício Kneipp <fabricio.kneipp@gmail.com>
2017-02-02 16:29:32 -0200

    Fix route namespaces

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
+
 Route::group(['middleware' => ['web']], function () {
-    Route::get('redirect/{provider}', 'Kneipp\Ward\Http\Controllers\SocialiteWrapperController@redirect');
-    Route::get('callback/{provider}', 'Kneipp\Ward\Http\Controllers\SocialiteWrapperController@callback');
+    Route::get('redirect/{provider}', 'Kneipp\SocialiteWrapper\Http\Controllers\SocialiteWrapperController@redirect');
+    Route::get('callback/{provider}', 'Kneipp\SocialiteWrapper\Http\Controllers\SocialiteWrapperController@callback');
 });
